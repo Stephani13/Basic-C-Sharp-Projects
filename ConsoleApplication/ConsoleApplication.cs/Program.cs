736c7b785e9025ac463d804734b86c5af28905f5
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-03-31 23:42:24 -0400

    Add files via upload

---
+++ ConsoleApplication/ConsoleApplication.cs/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApplication.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an integer:");
+            string number = Console.ReadLine();
+            long numberInput = Convert.ToInt64(number);
+
+            Console.WriteLine(numberInput * 50 );
+
+            Console.WriteLine(numberInput + 25);
+
+            Console.WriteLine(numberInput / 12.5);
+
+            Console.WriteLine(numberInput > 50);
+
+            Console.WriteLine(numberInput % 7);
+
+        }
+    }
+}
