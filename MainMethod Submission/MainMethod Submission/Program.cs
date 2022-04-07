60f83b5b82b0e066401786631f2a69f9c24f38ba
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 16:29:06 -0400

    Add files via upload

---
+++ MainMethod Submission/MainMethod Submission/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MainMethod_Submission
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //call on the methods and pass the arguments to be printed on the screen
+            Mathoperation Addition = new Mathoperation();
+
+            int addingResult = Mathoperation.MathOperation(30, 40);
+
+            Console.WriteLine(addingResult);
+
+            decimal multiplyResult = Mathoperation.MathOperation(3.99m, 1.5m);
+            Console.WriteLine(multiplyResult);
+
+            int resultSubstract = Mathoperation.MathOperation("2", "33");
+            int resultsubstract = Convert.ToInt32(resultSubstract);
+            Console.WriteLine(resultsubstract);
+        }
+    }
+}
