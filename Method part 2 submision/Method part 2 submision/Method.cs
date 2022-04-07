0e5429c780df0c6699a927527a3125f9d63fdebc
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 17:24:01 -0400

    Add files via upload

---
+++ Method part 2 submision/Method part 2 submision/Method.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_part_2_submision
+{
+    class Method
+    {
+        public int MathOperation(int number1, int number2 = 5)
+        {
+            return number1 * number2;
+        }
+    }
+}
---
+++ Method part 2 submision/Method part 2 submision/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Method_part_2_submision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method Operation = new Method();
+
+
+            Console.WriteLine("Please choose a number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please choose a second number. Press enter if you do not wish to enter one:");
+            string number = Console.ReadLine();
+            //an if statement in case the user does not insert a second number
+            if (number == "")
+            {
+                int result = Operation.MathOperation(number1);
+                Console.WriteLine(number1 + " multiplied by 5 equals " + result);
+            }
+            //otherwise replace the second number in the argument
+            else
+            {
+                int number2 = Convert.ToInt32(number);
+                int result = Operation.MathOperation(number1, number2);
+
+                Console.WriteLine(number2 + " multiplied by " + number2 + " equals " + result);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
