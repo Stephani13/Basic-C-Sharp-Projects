26e8fe6e49ea0d29c0a6e0fdd2b4c8ed86d078ec
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 18:51:33 -0400

    classes

---
+++ Class Submission/Class Submission/Methods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Submission
+{
+    class Methods
+    {
+        public static void MathOperations(int number, int number3)
+        {
+            
+            int result = (number - number3) / 2;
+            Console.WriteLine("(" + number + " minus " + number3 + ") divided by 2 equals " + result);
+        }
+
+        public static int Adding(out int i)
+        {
+            i = 20;
+            return i;
+        
+        }
+
+
+
+
+    }
+}
---
+++ Class Submission/Class Submission/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Class_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods operation = new Methods();
+
+            int i;
+            //asking user for input
+            Console.WriteLine("Please choose a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int number2 = Methods.Adding(out i);
+            //calling the class
+            Methods.MathOperations(number, number2 );
+        }
+    }
+}
