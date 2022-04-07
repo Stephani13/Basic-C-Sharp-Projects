d32ecbbdd28daf141733abfdf723b67ca05686ef
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 17:56:05 -0400

    Add files via upload

---
+++ Method part3 submission/Method part3 submission/Method.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_part3_submission
+{
+    class Method
+    {
+        public static void MathOperation(int number, int number2)
+        {
+            int result = number + number2;
+            Console.WriteLine(number + " + " + number2 + " = " + result);
+            return;
+        }
+    }
+}
---
+++ Method part3 submission/Method part3 submission/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Method_part3_submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Method Operations = new Method();
+            //creates to variables that holds the numbers
+            int number1 = 10;
+            int number2 = 29;
+            //call method from class Method
+            Method.MathOperation(number1, number2);
+        }
+    }
+}
