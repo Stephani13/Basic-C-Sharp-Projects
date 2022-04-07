d163f1fdb6a1c0cf669dc564499e1257acd24da0
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-04-06 21:08:33 -0400

    Add files via upload

---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System;
+
+namespace StringAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string one = "My brother ";
+            string two = "liam";
+            string three = ", he is 4 years old."
+            StringBuilder sb = new StringBuilder();
+
+
+            sb.Append("name is: ");
+            two = two.ToUpper();
+            Console.WriteLine(one + sb + two + three );
+            Console.ReadLine();
+        }
+    }
+}
