bed9ac7df54ba0525c57ea88bc844dd51e1fc369
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 02:16:14 -0400

    Add files via upload

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //create a list of numbers
+                List<int> numberList = new List<int>() { 3, 8, 6, 4, 90, 44, 33, 21 };
+                Console.WriteLine("Please choose a number.");
+                //ask for user input and use a loop to divide by the number chosen from the user
+                int usernumber = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numberList.Count; i++)
+                {
+                    int result = numberList[i] / usernumber;
+                    Console.WriteLine(result);
+                }
+
+
+            }
+            catch(FormatException ex)
+            {
+                //if number is invalid then a message would display
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
