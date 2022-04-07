8d71cb1729ed17609a6ce9d2dfb2594342c25e2f
Stephani13 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 10:24:43 -0400

    Add files via upload

---
+++ Calling Methods Submission/Calling Methods Submission/Methods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calling_Methods_Submission
+{
+    class Methods
+    {
+        //creating three methods that get passed a parameter
+        public int AddingTwoNumbers(int number, int number2)
+        {
+
+            return number + number2;
+        }
+
+        public int MultiplyingTwoNumbers(int number, int number2)
+        {
+            return number * number2;
+        }
+
+
+        public int Square(int number, int number2)
+        {
+            //using the Math.Pow method to exponentiate one number to the power of the other
+            int result = Convert.ToInt32(Math.Pow(number, number2));
+            return result;
+        }
+    }
+
+    
+}
---
+++ Calling Methods Submission/Calling Methods Submission/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Calling_Methods_Submission
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //calling the methods for each equation
+            Methods Addition = new Methods();
+
+            //asking the user input
+            Console.WriteLine("Please choose a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            //getting the user input and adding it to a fixed number
+            int addingResult = Addition.AddingTwoNumbers(number, 30);
+            Console.WriteLine(number + " + " + "30 " + "equals "  + addingResult);
+            //getting the user input and multiplying it with the result
+            int multiplyingResult = Addition.MultiplyingTwoNumbers(number, addingResult);
+            Console.WriteLine(addingResult + " multiplied by " + number + " equals " + multiplyingResult);
+            //getting the user input and the result of the multiplication squaring it with the input
+            int squaringResult = Addition.Square( multiplyingResult, number);
+            Console.WriteLine(multiplyingResult + " to the power of " + number + " equals " + squaringResult);
+
+        }
+    }
+}
