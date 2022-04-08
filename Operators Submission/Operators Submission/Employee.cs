cc184df73646094f14484d8105b76f3ff3be2dc2
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-04-07 21:46:20 -0400

    operators

--- Operators Submission/Operators Submission/Employee.cs
+++ Operators Submission/Operators Submission/Employee.cs
@@ -14,11 +14,11 @@ namespace Operators_Submission
 
         public static bool operator == (Employee employee1, Employee employee2)
         {
-            return (employee1.Id == employee2.Id);
+            return employee1.Id == employee2.Id;
         }
         public static bool operator !=(Employee employee1, Employee employee2)
         {
-            return !(employee1.Id == employee2.Id);
+            return employee1.Id != employee2.Id;
         }
     }
 }
--- Operators Submission/Operators Submission/Program.cs
+++ Operators Submission/Operators Submission/Program.cs
@@ -8,7 +8,13 @@ namespace Operators_Submission
         {
 
             Employee test = new Employee();
-            test.Id = new List<int>();
+            int employee1 = test.Id = 113;
+            int employee2 = test.Id = 114;
+
+            bool result = Employee.Equals(employee1, employee2);
+            Console.WriteLine(result);
+            
+            
 
         }
     }
