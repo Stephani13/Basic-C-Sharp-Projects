a4da6353c8a27304228fc3521b6fe70d446d70e1
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-04-08 23:20:42 -0400

    constructors

---
+++ Constructor Submission/Constructor Submission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Constructor_Submission
+{
+    class Program
+    {
+
+        public Program(): this(25)
+        {
+            //this is refering to the same class, so with the this() method it actually 
+            //executes the Program(int Age)
+        }
+
+        public Program(int Age)
+        {
+            //display variable to see the result
+            Console.Write("Age is: " + Age);
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            //creates a costant
+            const string name = "Massiel";
+            var name1 = "Louisa";
+        }
+    }
+}
