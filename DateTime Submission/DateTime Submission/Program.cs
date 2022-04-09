5c8681810299f412a407ec2a8b6116742d563e67
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-04-08 22:11:31 -0400

    datetime

---
+++ DateTime Submission/DateTime Submission/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DateTime_Submission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //get the current day and time
+            Console.WriteLine(DateTime.Now);
+            //ask user for input and stores it in a int type variable
+            Console.WriteLine("Please, Choose  number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            //displays to the console the time it will be in an X number of hours, X being the variable number 
+            //defined by the user
+            Console.WriteLine(DateTime.Now.AddHours(number));
+        }
+    }
+}
