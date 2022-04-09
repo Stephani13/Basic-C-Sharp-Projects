9e9b9a9ebe7160e73ca76f952024ea3149c552ed
Tiffy113 <94413603+Stephani13@users.noreply.github.com>
2022-04-08 21:11:56 -0400

    lambda update

--- Lambda submission/Lambda submission/Program.cs
+++ Lambda submission/Lambda submission/Program.cs
@@ -12,26 +12,36 @@ namespace Lambda_submission
 
 
 
-
-
             //use lambda to get employees with a first name of Joe
-            List<Employee> newList = listing.Employees.FindAll(x => x.FirstName == "Joe").ToList();
-            Console.WriteLine("Thesee are the employees with a first name of 'Joe':\n");
-            foreach (Employee employee in newList)
+            Console.WriteLine("Thesee are the employees with a first name of 'Joe':");
+
+
+            foreach (Employee employees in listing.Employees)
             {
-                Console.WriteLine(employee);
+                List<string> empty = new List<string>();
+                if (employees.FirstName == "Joe")
+                {
+                    empty.Add(employees.FirstName + employees.LastName);
+                    for (int i = 0; i < empty.Count; i++)
+                    {
+                        Console.WriteLine(empty[i]);
+                    }
+                }
             }
+            Console.WriteLine("\n\nThesee are the employees with a first name of 'Joe' using a lambda expression: ");
 
-            foreach (Employee employee in listing.Employees.FindAll(x => x.FirstName == "Joe"))
+            foreach (Employee employee1 in listing.Employees.Where(x => x.FirstName.Contains("Joe")))
             {
-                
+                List<string> empty1 = new List<string>();
+                empty1.Add(employee1.FirstName + employee1.LastName);
+                for (int i = 0; i < empty1.Count; i++)
+                {
+                    Console.WriteLine(empty1[i] );
+                }
             }
-
-
-
-
+            
             //gets the employees with an id greather than 5
-            Console.WriteLine("Employee with an Id greather than 5: \n");
+            Console.WriteLine("\n\nEmployee with an Id greather than 5: \n");
             foreach (Employee employee in listing.Employees.FindAll(x => x.Id > 5))
             {
                 
